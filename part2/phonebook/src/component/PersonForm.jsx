8a2dcfb3f522cf864c2f446d72357949fbd822e6
MarcoAR1 <marcorivero.mr26@gmail.com>
2021-04-06 17:47:28 -0300

    final part3

--- part2/phonebook/src/component/PersonForm.jsx
+++ part2/phonebook/src/component/PersonForm.jsx
@@ -7,20 +7,28 @@ const PersonForm = ({ persons, setPersons, notification }) => {
   const handleChangeName = (event) => {
     setNewName(event.target.value);
   };
-
+  const resetFields = () => {
+    setNewName("");
+    setNewNumber("");
+  };
   const handleChangeNumber = (event) => {
     setNewNumber(event.target.value);
   };
 
   const handleSumit = (event) => {
+    const fixNewName = newName
+      .split(" ")
+      .filter((e) => e)
+      .join(" ");
+    console.log(fixNewName);
     event.preventDefault();
-    if (newName && newNumber) {
+    if (fixNewName && newNumber) {
       const contact = {
-        name: newName.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase()),
+        name: fixNewName.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase()),
         number: newNumber,
       };
       const find = persons.find(
-        ({ name }) => name.toLowerCase() === newName.toLowerCase()
+        ({ name }) => name.toLowerCase() === fixNewName.toLowerCase()
       );
       find
         ? replaceContact(find.id, contact)
@@ -30,25 +38,23 @@ const PersonForm = ({ persons, setPersons, notification }) => {
                 return [...prevPersons, data.data];
               });
               notification(`Added ${data.data.name}`);
+              resetFields();
             })
-            .catch(() => {
-              notification(
-                `We could not create the contact ${newName} try again.`,
-                true
-              );
-            });
-      setNewName("");
-      setNewNumber("");
+            .catch((err) => notification(`${err.response.data.error}`, true));
     } else {
-      newName
+      fixNewName
         ? alert("Please, fill in the missing field number.")
         : alert("Please, fill in the missing field name.");
     }
   };
 
   const replaceContact = (contact_id, contact) => {
+    const fixNewName = newName
+      .split(" ")
+      .filter((e) => e)
+      .join(" ");
     window.confirm(
-      `${newName} is already added to phonebook, replace the old number with a new one?`
+      `${fixNewName} is already added to phonebook, replace the old number with a new one?`
     )
       ? UpdatePersons(contact_id, contact)
           .then((res) => {
@@ -60,13 +66,11 @@ const PersonForm = ({ persons, setPersons, notification }) => {
                 })
                 .concat(data);
             });
-            notification(`Information of ${newName} has replace successful`);
+            notification(`Information of ${fixNewName} has replace successful`);
+            resetFields();
           })
-          .catch(() => {
-            notification(
-              `Information of ${newName} has already been removed from server`,
-              true
-            );
+          .catch((err) => {
+            notification(`${err.response.data.error}`, true);
           })
       : alert("Ok,Don`t update contact.");
   };
